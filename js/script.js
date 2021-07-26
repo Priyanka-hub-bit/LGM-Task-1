854ac80ca054ba9ad774fee329e050d1bfe17182
Priyanka-06 <shivpriya@386gmail.com>
2021-07-26 11:58:55 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.burger-menu').click(function(event) {
+        $('.burger-menu,.main-nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+ });
